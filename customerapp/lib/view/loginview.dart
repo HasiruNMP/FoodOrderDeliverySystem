af5c049769a78a484bf298097ab8c3fcc9c13b23
Rakshitha <rakshithadilshan1@gmail.com>
2022-02-16 12:04:30 +0530

    create sign up ui

--- customerapp/lib/view/loginview.dart
+++ customerapp/lib/view/loginview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
+  static String id = 'login';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
