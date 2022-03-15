ac64805a18dd9c9d916a85fb59256ee51b5e8866
Ravindu Arsakulasooriya <56332270+Ravindu78@users.noreply.github.com>
2022-03-15 18:09:29 +0530

    order history1

--- deliveryapp/lib/main.dart
+++ deliveryapp/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:deliveryapp/view/homeview.dart';
 import 'package:deliveryapp/view/loginview.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- deliveryapp/lib/view/historyview.dart
+++ deliveryapp/lib/view/historyview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class HistoryView extends StatefulWidget {
   const HistoryView({Key? key}) : super(key: key);
@@ -10,7 +12,190 @@ class HistoryView extends StatefulWidget {
 class _HistoryViewState extends State<HistoryView> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
+      appBar: AppBar(
+        title: Text('Orders History'),
+      ),
+      body: SafeArea(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("orders")
+              .snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting ||
+                !snapshot.hasData) {
+              //  return CircularProgressIndicator();
+            }
+
+            if (snapshot.hasData) {
+              print('has data');
+              return Container(
+                height: 529,
+                color: Colors.white,
+                child: ListView.builder(
+
+                  itemCount: snapshot.data!.docs.length,
+
+                  // ignore: missing_return
+                  itemBuilder: (BuildContext context, index) {
+                    QueryDocumentSnapshot category =
+                    snapshot.data!.docs[index];
+                    GeoPoint cLocation=category['customerLocation'];
+                    String cName=category['customerName'];
+                    String cPhone=category['customerPhone'];
+                    String deliveryPerson=category['deliveryPerson'];
+                    //String itemCounts=category['itemCounts'];
+                    String itemID=category['orderid'];
+                    Timestamp orderTime=category['orderTime'];
+                    String orderId=category['orderid'];
+                    String  totalPrice=category['totalPrice'];
+
+
+                    return ListTile(
+                      title: Card(
+                        child: TextButton(
+                          onPressed: () {
+                            // showDialog(
+                            //     context: context,
+                            //     builder: (context) {
+                            //       return const AlertDialog(
+                            //         title: Text('test'),
+                            //       );
+                            //     });
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        flex: 2,
+                                        child: Text(
+                                          'OderID',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.black),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                          ':',
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 4,
+                                        child: Text(
+                                          itemID.toString(),
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding:  EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children:  [
+                                      Expanded(
+                                          flex: 2,
+                                          child: Text('Time',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black))),
+                                      Expanded(
+                                        child: Text(
+                                          ':',
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 4,
+                                        child: Text(
+                                          orderTime.toString(),
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding:  EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children:  [
+                                      Expanded(
+                                          flex: 2,
+                                          child: Text('Name',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black))),
+                                      Expanded(
+                                        child: Text(
+                                          ':',
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 4,
+                                        child: Text(
+                                          cName.toString(),
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              );
+            }
+
+            return const SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+//+====
+class HistoryView1 extends StatefulWidget {
+  const HistoryView1({Key? key}) : super(key: key);
+
+  @override
+  _HistoryView1State createState() => _HistoryView1State();
+}
+
+class _HistoryView1State extends State<HistoryView1> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
       appBar: AppBar(
         title: Text('Orders History'),
       ),
@@ -55,7 +240,7 @@ class _HistoryViewState extends State<HistoryView> {
                               Expanded(
                                 flex: 4,
                                 child: Text(
-                                  '0003',
+                                  'orderId',
                                   style: TextStyle(color: Colors.black),
                                 ),
                               ),
