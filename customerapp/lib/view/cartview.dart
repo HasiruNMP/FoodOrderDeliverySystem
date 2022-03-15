fc7830d6def028c7d53e4caa1b2564b46a56b90b
Rakshitha <rakshithadilshan1@gmail.com>
2022-03-15 19:40:53 +0530

    Update cartview.dart

--- customerapp/lib/view/cartview.dart
+++ customerapp/lib/view/cartview.dart
@@ -21,132 +21,168 @@ class _CartViewState extends State<CartView> {
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 8,
-                  child: ListView(
+            child: Cart.basketItems.length == 0
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Center(
+                          child: Icon(
+                        Icons.report_gmailerrorred_outlined,
+                        size: 100,
+                        color: Colors.blue,
+                      )),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                        child: Text(
+                          'No Items in Your Cart',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Text(
+                            'Add some items! ',
+                            style: TextStyle(fontSize: 16, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                : Column(
                     children: [
-                      ListView.builder(
-                        shrinkWrap: true,
-                        primary: false,
-                        itemCount: Cart.basketItems.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            height: (MediaQuery.of(context).size.height) / 6,
-                            child: Card(
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    flex: 1,
-                                    child: Image.network(
-                                        Cart.basketItems[index].imgUrl),
-                                  ),
-                                  Expanded(
-                                    flex: 2,
-                                    child: Column(
+                      Expanded(
+                        flex: 8,
+                        child: ListView(
+                          children: [
+                            ListView.builder(
+                              shrinkWrap: true,
+                              primary: false,
+                              itemCount: Cart.basketItems.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  height:
+                                      (MediaQuery.of(context).size.height) / 6,
+                                  child: Card(
+                                    child: Row(
                                       children: [
                                         Expanded(
                                           flex: 1,
-                                          child: Container(
-                                            alignment: Alignment.topRight,
-                                            child: IconButton(
-                                              icon: Icon(Icons.close),
-                                              onPressed: () {
-                                                cart.remove(
-                                                    Cart.basketItems[index]);
-                                              },
-                                            ),
-                                          ),
-                                        ),
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(
-                                              alignment: Alignment.centerLeft,
-                                              child: Text(
-                                                Cart.basketItems[index].name,
-                                              )),
+                                          child: Image.network(
+                                              Cart.basketItems[index].imgUrl),
                                         ),
                                         Expanded(
                                           flex: 2,
-                                          child: Container(
-                                            alignment: Alignment.centerLeft,
-                                            child: Row(
-                                              children: [
-                                                IconButton(
+                                          child: Column(
+                                            children: [
+                                              Expanded(
+                                                flex: 1,
+                                                child: Container(
+                                                  alignment: Alignment.topRight,
+                                                  child: IconButton(
+                                                    icon: Icon(Icons.close),
                                                     onPressed: () {
-                                                      setState(() {
-                                                        cart.updateProduct(
-                                                            Cart.basketItems[
-                                                                index],
-                                                            Cart
-                                                                    .basketItems[
-                                                                        index]
-                                                                    .quantity -
-                                                                1);
-                                                      });
+                                                      cart.remove(Cart
+                                                          .basketItems[index]);
                                                     },
-                                                    icon: const Icon(
-                                                        Icons.remove)),
-                                                Text(
-                                                  Cart.basketItems[index]
-                                                      .quantity
-                                                      .toString(),
+                                                  ),
                                                 ),
-                                                IconButton(
-                                                    onPressed: () {
-                                                      setState(() {
-                                                        cart.updateProduct(
-                                                            Cart.basketItems[
-                                                                index],
-                                                            Cart
-                                                                    .basketItems[
-                                                                        index]
-                                                                    .quantity +
-                                                                1);
-                                                      });
-                                                    },
-                                                    icon: Icon(Icons.add_box)),
-                                              ],
-                                            ),
+                                              ),
+                                              Expanded(
+                                                flex: 1,
+                                                child: Container(
+                                                    alignment:
+                                                        Alignment.centerLeft,
+                                                    child: Text(
+                                                      Cart.basketItems[index]
+                                                          .name,
+                                                    )),
+                                              ),
+                                              Expanded(
+                                                flex: 2,
+                                                child: Container(
+                                                  alignment:
+                                                      Alignment.centerLeft,
+                                                  child: Row(
+                                                    children: [
+                                                      IconButton(
+                                                          onPressed: () {
+                                                            setState(() {
+                                                              cart.updateProduct(
+                                                                  Cart.basketItems[
+                                                                      index],
+                                                                  Cart.basketItems[index]
+                                                                          .quantity -
+                                                                      1);
+                                                            });
+                                                          },
+                                                          icon: const Icon(
+                                                              Icons.remove)),
+                                                      Text(
+                                                        Cart.basketItems[index]
+                                                            .quantity
+                                                            .toString(),
+                                                      ),
+                                                      IconButton(
+                                                          onPressed: () {
+                                                            setState(() {
+                                                              cart.updateProduct(
+                                                                  Cart.basketItems[
+                                                                      index],
+                                                                  Cart.basketItems[index]
+                                                                          .quantity +
+                                                                      1);
+                                                            });
+                                                          },
+                                                          icon: Icon(
+                                                              Icons.add_box)),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                              Expanded(
+                                                flex: 1,
+                                                child: Container(
+                                                    alignment:
+                                                        Alignment.centerLeft,
+                                                    child: Text(
+                                                        'Rs.${Cart.basketItems[index].price.toString()}')),
+                                              ),
+                                            ],
                                           ),
-                                        ),
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(
-                                              alignment: Alignment.centerLeft,
-                                              child: Text(
-                                                  'Rs.${Cart.basketItems[index].price.toString()}')),
                                         ),
                                       ],
                                     ),
                                   ),
-                                ],
-                              ),
+                                );
+                              },
                             ),
-                          );
-                        },
+                          ],
+                        ),
                       ),
+                      Expanded(
+                          flex: 1,
+                          child: Card(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                    "Total Price: Rs.${Cart.totalPrice.toString()}"),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(context, 'checkout');
+                                    },
+                                    child: const Text("Checkout")),
+                              ],
+                            ),
+                          )),
                     ],
                   ),
-                ),
-                Expanded(
-                    flex: 1,
-                    child: Card(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Total Price: Rs.${Cart.totalPrice.toString()}"),
-                          ElevatedButton(
-                              onPressed: () {
-                                Navigator.pushNamed(context, 'checkout');
-                              },
-                              child: const Text("Checkout")),
-                        ],
-                      ),
-                    )),
-              ],
-            ),
           ),
         ),
       );
