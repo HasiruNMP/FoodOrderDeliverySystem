57565ab5d4807b9dcf8b39ae03348fbae569fab2
Ravindu Arsakulasooriya <56332270+Ravindu78@users.noreply.github.com>
2022-03-15 19:44:08 +0530

    history view / order view

--- deliveryapp/lib/view/orderview.dart
+++ deliveryapp/lib/view/orderview.dart
@@ -1,6 +1,5 @@
 import 'package:deliveryapp/view/deliverview.dart';
-import 'package:deliveryapp/view/historyview.dart';
-import 'package:deliveryapp/view/profileview.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderView extends StatefulWidget {
@@ -18,136 +17,194 @@ class _OrderViewState extends State<OrderView> {
         title: Text('Pending Orders'),
       ),
       body: SafeArea(
-        child: ListView(
-          children: [
-            ListTile(
-              title: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: const [
-                            Expanded(
-                              flex: 2,
-                              child: Text(
-                                'Order No.',
-                                style: TextStyle(fontWeight: FontWeight.bold),
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("orders")
+              .snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting ||
+                !snapshot.hasData) {
+              //  return CircularProgressIndicator();
+            }
+
+            if (snapshot.hasData) {
+              print('has data');
+              return Container(
+                height: 529,
+
+
+                color: Colors.white,
+                child: ListView.builder(
+
+                  itemCount: snapshot.data!.docs.length,
+
+                  // ignore: missing_return
+                  itemBuilder: (BuildContext context, index) {
+                    QueryDocumentSnapshot category =
+                    snapshot.data!.docs[index];
+                    GeoPoint cLocation=category['customerLocation'];
+                    String cName=category['customerName'];
+                    String cPhone=category['customerPhone'];
+                    String deliveryPerson=category['deliveryPerson'];
+                    //String itemCounts=category['itemCounts'];
+                    String itemID=category['orderid'];
+                    Timestamp orderTime=category['orderTime'];
+                    String orderId=category['orderid'];
+                    String  totalPrice=category['totalPrice'];
+
+
+                    return ListTile(
+                      title: Padding(
+                        padding:  EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Card(
+                                child: Padding(
+                                  padding:  EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding:  EdgeInsets.all(8.0),
+                                        child: Row(
+                                          children:  [
+                                            Expanded(
+                                              flex: 2,
+                                              child: Text(
+                                                'Order No.',
+                                                style: TextStyle(fontWeight: FontWeight.bold),
+                                              ),
+                                            ),
+                                            Expanded(child: Text(':')),
+                                            Expanded(flex: 4, child: Text(itemID.toString())),
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:  EdgeInsets.all(8.0),
+                                        child: Row(
+                                          children:  [
+                                            Expanded(
+                                                flex: 2,
+                                                child: Text('Time',
+                                                    style:
+                                                    TextStyle(fontWeight: FontWeight.bold))),
+                                            Expanded(child: Text(':')),
+                                            Expanded(
+                                                flex: 4, child: Text(orderTime.toString())),
+                                          ],
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Expanded(
+                                            flex: 3,
+                                            child: OutlinedButton(
+                                              onPressed: () {},
+                                              child: const Text('DELIVERED'),
+                                            ),
+                                          ),
+                                          const Expanded(child: SizedBox()),
+                                          Expanded(
+                                            flex: 3,
+                                            child: OutlinedButton(
+                                              onPressed: () {
+                                                Navigator.push(context, MaterialPageRoute(builder: (context) {return const DeliverView();},),);
+                                              },
+                                              child: const Text('VIEW'),
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
                               ),
                             ),
-                            Expanded(child: Text(':')),
-                            Expanded(flex: 4, child: Text('0003')),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: const [
-                            Expanded(
-                                flex: 2,
-                                child: Text('Time',
-                                    style:
-                                    TextStyle(fontWeight: FontWeight.bold))),
-                            Expanded(child: Text(':')),
-                            Expanded(
-                                flex: 4, child: Text('3rd Jan 2022, 12.45pm')),
-                          ],
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: OutlinedButton(
-                              onPressed: () {},
-                              child: const Text('DELIVERED'),
-                            ),
-                          ),
-                          const Expanded(child: SizedBox()),
-                          Expanded(
-                            flex: 3,
-                            child: OutlinedButton(
-                              onPressed: () {
-                                Navigator.push(context, MaterialPageRoute(builder: (context) {return const DeliverView();},),);
-                              },
-                              child: const Text('VIEW'),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              title: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: const [
-                            Expanded(
-                              flex: 2,
-                              child: Text(
-                                'Order No.',
-                                style: TextStyle(fontWeight: FontWeight.bold),
+                            ListTile(
+                              title: Card(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding:  EdgeInsets.all(8.0),
+                                        child: Row(
+                                          children:  [
+                                            Expanded(
+                                              flex: 2,
+                                              child: Text(
+                                                'Order No.',
+                                                style: TextStyle(fontWeight: FontWeight.bold),
+                                              ),
+                                            ),
+                                            Expanded(child: Text(':')),
+                                            Expanded(flex: 4, child: Text(itemID.toString())),
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:  EdgeInsets.all(8.0),
+                                        child: Row(
+                                          children: [
+                                            Expanded(
+                                                flex: 2,
+                                                child: Text('Time',
+                                                    style:
+                                                    TextStyle(fontWeight: FontWeight.bold))),
+                                            Expanded(child: Text(':')),
+                                            Expanded(
+                                                flex: 4, child: Text(orderTime.toString())),
+                                          ],
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Expanded(
+                                            flex: 3,
+                                            child: OutlinedButton(
+                                              onPressed: () {},
+                                              child: const Text('DELIVERED'),
+                                            ),
+                                          ),
+                                          const Expanded(child: SizedBox()),
+                                          Expanded(
+                                            flex: 3,
+                                            child: OutlinedButton(
+                                              onPressed: () {},
+                                              child: const Text('VIEW'),
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
                               ),
-                            ),
-                            Expanded(child: Text(':')),
-                            Expanded(flex: 4, child: Text('0003')),
+                            )
                           ],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: const [
-                            Expanded(
-                                flex: 2,
-                                child: Text('Time',
-                                    style:
-                                    TextStyle(fontWeight: FontWeight.bold))),
-                            Expanded(child: Text(':')),
-                            Expanded(
-                                flex: 4, child: Text('3rd Jan 2022, 12.45pm')),
-                          ],
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: OutlinedButton(
-                              onPressed: () {},
-                              child: const Text('DELIVERED'),
-                            ),
-                          ),
-                          const Expanded(child: SizedBox()),
-                          Expanded(
-                            flex: 3,
-                            child: OutlinedButton(
-                              onPressed: () {},
-                              child: const Text('VIEW'),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+                    );
+                  },
                 ),
+              );
+            }
+
+            return const SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: CircularProgressIndicator(),
               ),
-            )
-          ],
+            );
+          },
         ),
       ),
     );
   }
 }
-
