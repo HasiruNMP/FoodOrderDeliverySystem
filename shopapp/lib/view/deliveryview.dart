2286176d8a77d5bc34ff385bbe655dcd2147ab1b
Ravindu Jayathilaka <57854539+Ravindu-Jayathilaka@users.noreply.github.com>
2022-03-15 16:17:53 +0530

    listview error

--- shopapp/lib/view/deliveryview.dart
+++ shopapp/lib/view/deliveryview.dart
@@ -11,61 +11,66 @@ class _DeliveryViewState extends State<DeliveryView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Column(
-            children: [
-              Container(
-                child: Center(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Add'),
+      body: SafeArea(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Container(
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Add'),
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Card(
-                      child: Text('Name'),
+                Expanded(
+                  child: SizedBox(
+                    height: 200.0,
+                    child: ListView(
+                      children: [
+                        Card(
+                          child: Text('Name'),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          VerticalDivider(
-            color: Colors.black26,
-          ),
-          Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Name'),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Username'),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Password'),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Phone Number'),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'NIC'),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'License'),
-              ),
-            ],
-          )
-        ],
+              ],
+            ),
+            VerticalDivider(
+              color: Colors.black26,
+            ),
+            Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Name'),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Username'),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Password'),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Phone Number'),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'NIC'),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'License'),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
