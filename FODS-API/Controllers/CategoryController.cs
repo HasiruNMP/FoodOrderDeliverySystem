5eb2d6b562416684d9a3829edb54d948bb3fa31d
Dinith Tennakoon <53987995+iamdinith@users.noreply.github.com>
2022-04-19 00:09:29 +0530

    Dinith - Category, Product and Order controllers

--- FODS-API/Controllers/CategoryController.cs
+++ FODS-API/Controllers/CategoryController.cs
@@ -10,21 +10,40 @@ using FODS_API.Models;
 
 namespace FODS_API.Controllers
 {
-    [Route("orders")]
+    [Route("api/[controller]")]
     [ApiController]
-    public class OrderController : ControllerBase
+    public class CategoryController : ControllerBase
     {
-
         private readonly IConfiguration _configuration;
-        public OrderController(IConfiguration configuration)
+        public CategoryController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
-
-         [HttpGet, Route("getneworders")]
-        public JsonResult GetNewOrders()
+        [HttpPost, Route("postcategory")]
+        public JsonResult PostCategory( string Name, string ImgUrl)
         {
-            string query = @"SELECT * FROM [dbo].[ORDERS] WHERE IsProcessed = 0";
+            string query = @"insert into [dbo].[CATEGORIES] values('" + Name + "','" + ImgUrl + "')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpGet, Route("getcategories")]
+        public JsonResult GetCategories()
+        {
+            string query = @"SELECT * FROM [dbo].[CATEGORIES]";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("FODSDB");
             SqlDataReader myReader;
@@ -42,73 +61,10 @@ namespace FODS_API.Controllers
             return new JsonResult(table);
         }
 
-        [HttpGet, Route("getprocessedorders")]
-        public JsonResult GetProcessedOrders()
+        [HttpPut, Route("putcategorydetails")]
+        public JsonResult PutCategoryDetails(int CategoryId, string Name, string ImgUrl)
         {
-            string query = @"SELECT * FROM [dbo].[ORDERS] WHERE IsProcessed != 0 AND (IsReceived = 0 OR IsDelivered = 0)";
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();
-                }
-            }
-            return new JsonResult(table);
-        }
-
-        [HttpGet, Route("getcompletedorders")]
-        public JsonResult GetCompletedOrders()
-        {
-            string query = @"SELECT * FROM [dbo].[ORDERS] WHERE IsReceived != 0 AND IsDelivered != 0";
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();
-                }
-            }
-            return new JsonResult(table);
-        }
-
-        [HttpGet, Route("getorderitems")]
-        public JsonResult GetOrderItems(int orderId)
-        {
-            string query = @"SELECT * FROM [dbo].[ORDER_ITEMS] WHERE OrderId = " + orderId;
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();
-                }
-            }
-            return new JsonResult(table);
-        }
-
-        [HttpPut,Route("putprocessed")]
-        public JsonResult PutProcessed(int orderId)
-        {
-            string query = @"UPDATE [dbo].[ORDERS] SET IsProcessed = 1 WHERE OrderId =" + orderId;
+            string query = @"UPDATE [dbo].[CATEGORIES] SET Name = '" + Name + "',ImgUrl = '" + ImgUrl + "' WHERE CategoryId =" + CategoryId;
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("FODSDB");
             SqlDataReader myReader;
@@ -126,5 +82,26 @@ namespace FODS_API.Controllers
             return new JsonResult("Updated Successfully");
         }
 
+        [HttpDelete]
+        public JsonResult DeleteCategory(int CategoryId)
+        {
+            string query = @"delete from dbo.CATEGORIES where CategoryId=" + CategoryId;
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully!");
+        }
     }
 }
--- FODS-API/Controllers/ProductController.cs
+++ FODS-API/Controllers/ProductController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using FODS_API.Models;
 
 namespace FODS_API.Controllers
 {
@@ -14,6 +19,91 @@ namespace FODS_API.Controllers
         public ProductController(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+
+        [HttpPost,Route("postproduct")]
+        public JsonResult PostProduct(int CategoryId,string Name,string Description,float Price,string ImgUrl)
+        {
+            string query = @"insert into [dbo].[PRODUCTS] values("+CategoryId+",'"+Name+"','"+Description+"',"+Price+",'"+ImgUrl+"')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+
+         [HttpGet, Route("getcategoryproducts")]
+        public JsonResult GetCategoryProducts(int categoryId)
+        {
+            string query = @"SELECT * FROM [dbo].[PRODUCTS] WHERE CategoryId =" + categoryId;
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+         [HttpPut,Route("putproductdetails")]
+        public JsonResult PutProductDetails(int ProductId,int CategoryId,string Name,string Description,float Price,string ImgUrl)
+        {
+            string query = @"UPDATE [dbo].[PRODUCTS] SET CategoryId = " + CategoryId + ",Name = '" + Name + "',Description = '" + Description + "',Price = " + Price + ",ImgUrl = '" + ImgUrl + "' WHERE ProductId =" + ProductId;
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+
+         [HttpDelete]
+        public JsonResult DeleteProduct(int ProductId)
+        {
+            string query = @"delete from dbo.PRODUCTS where ProductId="+ProductId;
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("FODSDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+               
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully!");
         }
 
     }
